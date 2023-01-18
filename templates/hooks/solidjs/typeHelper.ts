ea0ee47c67b4ac65f0e171d34c651692ee7aefca
Olli Paloviita <olli.paloviita@gmail.com>
2023-01-18 19:50:13 +0200

    Unnecessary errors suppressed

--- templates/hooks/solidjs/typeHelper.ts
+++ templates/hooks/solidjs/typeHelper.ts
@@ -1,3 +1,4 @@
+// @ts-expect-error template_name gets replaced
 import * as rustAll from "template_name_rust";
 // eslint-disable-next-line no-unused-vars
 const { default: _, initSync: __, ...rustRest } = rustAll;
